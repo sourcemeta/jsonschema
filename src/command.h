b1f3b3ac8dbe7324a4ec176533b272369bb74a0f
Juan Cruz Viotti <jv@jviotti.com>
2024-05-27 11:11:50 +0100

    Inline the help command
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/command.h
+++ src/command.h
@@ -5,7 +5,6 @@
 #include <string> // std::string
 
 namespace intelligence::jsonschema::cli {
-auto help(const std::string &program) -> int;
 auto fmt(const std::span<const std::string> &arguments) -> int;
 auto frame(const std::span<const std::string> &arguments) -> int;
 auto lint(const std::span<const std::string> &arguments) -> int;
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,45 @@
-#include <algorithm> // std::min
-#include <cstdlib>   // EXIT_FAILURE
-#include <iostream>  // std::cerr
-#include <span>      // std::span
-#include <string>    // std::string
-#include <vector>    // std::vector
+#include <algorithm>   // std::min
+#include <cstdlib>     // EXIT_FAILURE, EXIT_SUCCESS
+#include <filesystem>  // std::filesystem
+#include <iostream>    // std::cerr, std::cout
+#include <span>        // std::span
+#include <string>      // std::string
+#include <string_view> // std::string_view
+#include <vector>      // std::vector
 
 #include "command.h"
+#include "configure.h"
+
+constexpr std::string_view USAGE_DETAILS{R"EOF(
+Commands:
+
+   validate <schema.json> <instance.json>
+
+       Validate an instance against a schema, printing error information, if
+       any, in a human-readable manner.
+
+   fmt [schema.json...] [--check/-c]
+
+       Format the input schemas in-place. Passing directories as input means
+       to format every `.json` file in such directory (recursively). If no
+       argument is passed, format every `.json` file in the current working
+       directory (recursively). The `--check/-c` option will check if the given
+       schemas adhere to the desired formatting without modifying them.
+
+   lint [schema.json...] [--fix/-f]
+
+       Lint the input schemas. Passing directories as input means to lint
+       every `.json` file in such directory (recursively). If no argument is
+       passed, lint every `.json` file in the current working directory
+       (recursively). The `--fix/-f` option will attempt to automatically
+       fix the linter errors.
+
+   frame <schema.json>
+
+       Frame a schema in-place, displaying schema locations and references
+       in a human-readable manner.
+
+)EOF"};
 
 auto jsonschema_main(const std::string &program, const std::string &command,
                      const std::span<const std::string> &arguments) -> int {
@@ -18,7 +52,12 @@ auto jsonschema_main(const std::string &program, const std::string &command,
   } else if (command == "validate") {
     return intelligence::jsonschema::cli::validate(arguments);
   } else {
-    return intelligence::jsonschema::cli::help(program);
+    std::cout << "JSON Schema CLI - v"
+              << intelligence::jsonschema::cli::PROJECT_VERSION << "\n";
+    std::cout << "Usage: " << std::filesystem::path{program}.filename().string()
+              << " <command> [arguments...]\n";
+    std::cout << USAGE_DETAILS;
+    return EXIT_SUCCESS;
   }
 }
 
