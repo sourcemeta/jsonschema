f81a144f22ed3bdf5f7c4f1b1025326e0b5f9a2f
Juan Cruz Viotti <jv@jviotti.com>
2024-05-31 21:10:22 -0300

    Add an `--http` option to `validate`, `test`, and `bundle` (#19)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/command_bundle.cc
+++ src/command_bundle.cc
@@ -9,11 +9,12 @@
 
 auto intelligence::jsonschema::cli::bundle(
     const std::span<const std::string> &arguments) -> int {
-  const auto options{parse_options(arguments, {})};
+  const auto options{parse_options(arguments, {"h", "http"})};
   CLI_ENSURE(!options.at("").empty(), "You must pass a JSON Schema as input");
   auto schema{sourcemeta::jsontoolkit::from_file(options.at("").front())};
   sourcemeta::jsontoolkit::bundle(
-      schema, sourcemeta::jsontoolkit::default_schema_walker, resolver(options))
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      resolver(options, options.contains("h") || options.contains("http")))
       .wait();
   sourcemeta::jsontoolkit::prettify(
       schema, std::cout, sourcemeta::jsontoolkit::schema_format_compare);
