7719f444c00fd6b45dfdedc797a87ef96093cd82
Juan Cruz Viotti <jv@jviotti.com>
2024-06-07 15:30:50 -0400

    Support JSON Schema Draft 6 on `test` and `validate`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/validate_pass_draft6.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.json"
+{
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "properties": {
+    "foo": {
+      "type": "string"
+    }
+  }
+}
+EOF
+
+cat << 'EOF' > "$TMP/instance.json"
+{ "foo": "bar" }
+EOF
+
+"$1" validate "$TMP/schema.json" "$TMP/instance.json"
