fe3444162ac1c6bcbc8ac77e1464cdcac2b25fcc
Juan Cruz Viotti <jv@jviotti.com>
2024-06-12 19:58:51 -0400

    Improve how `test` displays metaschema checks (#86)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/test_single_pass_with_metaschema.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/test.json"
+{
+  "description": "My sample suite",
+  "schema": "http://json-schema.org/draft-04/schema#",
+  "tests": [
+    {
+      "description": "First test",
+      "valid": true,
+      "data": {}
+    },
+    {
+      "description": "Invalid type",
+      "valid": false,
+      "data": { "type": 1 }
+    }
+  ]
+}
+EOF
+
+"$1" test "$TMP/test.json" --metaschema > "$TMP/out.txt"
+
+cat << EOF > "$TMP/expected.txt"
+$TMP/test.json
+    Metaschema - http://json-schema.org/draft-04/schema#
+        PASS
+    My sample suite - First test
+        PASS
+    My sample suite - Invalid type
+        PASS
+EOF
+
+diff "$TMP/out.txt" "$TMP/expected.txt"
