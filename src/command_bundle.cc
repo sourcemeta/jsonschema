07ea1bf612b4e596d0ae36ea6494aa08d59d83c5
Juan Cruz Viotti <jv@jviotti.com>
2024-06-17 17:40:38 -0400

    Add a `--without-id`/`-w` option to bundle without identifiers (#90)
    See: https://github.com/asyncapi/spec-json-schemas/issues/550
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/command_bundle.cc
+++ src/command_bundle.cc
@@ -9,13 +9,26 @@
 
 auto intelligence::jsonschema::cli::bundle(
     const std::span<const std::string> &arguments) -> int {
-  const auto options{parse_options(arguments, {"h", "http"})};
+  const auto options{
+      parse_options(arguments, {"h", "http", "w", "without-id"})};
   CLI_ENSURE(!options.at("").empty(), "You must pass a JSON Schema as input");
   auto schema{sourcemeta::jsontoolkit::from_file(options.at("").front())};
-  sourcemeta::jsontoolkit::bundle(
-      schema, sourcemeta::jsontoolkit::default_schema_walker,
-      resolver(options, options.contains("h") || options.contains("http")))
-      .wait();
+
+  if (options.contains("w") || options.contains("without-id")) {
+    log_verbose(options) << "Bundling without using identifiers\n";
+    sourcemeta::jsontoolkit::bundle(
+        schema, sourcemeta::jsontoolkit::default_schema_walker,
+        resolver(options, options.contains("h") || options.contains("http")),
+        sourcemeta::jsontoolkit::BundleOptions::WithoutIdentifiers)
+        .wait();
+  } else {
+    sourcemeta::jsontoolkit::bundle(
+        schema, sourcemeta::jsontoolkit::default_schema_walker,
+        resolver(options, options.contains("h") || options.contains("http")),
+        sourcemeta::jsontoolkit::BundleOptions::Default)
+        .wait();
+  }
+
   sourcemeta::jsontoolkit::prettify(
       schema, std::cout, sourcemeta::jsontoolkit::schema_format_compare);
   std::cout << std::endl;
---
+++ test/bundle_remote_directory_without_id.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.json"
+{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://example.com",
+  "$ref": "nested"
+}
+EOF
+
+mkdir "$TMP/schemas"
+cat << 'EOF' > "$TMP/schemas/remote.json"
+{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://example.com/nested",
+  "type": "string"
+}
+EOF
+
+"$1" bundle "$TMP/schema.json" --resolve "$TMP/schemas" --without-id > "$TMP/result.json"
+
+cat << 'EOF' > "$TMP/expected.json"
+{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$ref": "#/$defs/https%3A~1~1example.com~1nested",
+  "$defs": {
+    "https://example.com/nested": {
+      "$schema": "https://json-schema.org/draft/2020-12/schema",
+      "type": "string"
+    }
+  }
+}
+EOF
+
+diff "$TMP/result.json" "$TMP/expected.json"
