9709a0c61343f1cd5721a63a70a1120cb149749e
Juan Cruz Viotti <jv@jviotti.com>
2024-06-18 21:35:24 -0400

    Show validation error on test failures (#101)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/test_single_fail.sh
+++ test/test_single_fail.sh
@@ -26,12 +26,36 @@ cat << 'EOF' > "$TMP/test.json"
 }
 EOF
 
-"$1" test "$TMP/test.json" && CODE="$?" || CODE="$?"
-
+"$1" test "$TMP/test.json" 1> "$TMP/output.txt" 2>&1 && CODE="$?" || CODE="$?"
 if [ "$CODE" = "0" ]
 then
   echo "FAIL" 1>&2
   exit 1
-else
-  echo "PASS" 1>&2
 fi
+
+cat << EOF > "$TMP/expected.txt"
+$(realpath "$TMP")/test.json
+    My sample suite - First test
+        PASS
+    My sample suite - Invalid type, expected to fail
+        FAIL
+
+error: The target document is expected to be one of the given values
+  at instance location "/type"
+  at evaluate path "/properties/type/anyOf/0/\$ref/enum"
+error: Mark the current position of the evaluation process for future jumps
+  at instance location "/type"
+  at evaluate path "/properties/type/anyOf/0/\$ref"
+error: The target document is expected to be of the given type
+  at instance location "/type"
+  at evaluate path "/properties/type/anyOf/1/type"
+error: The target is expected to match at least one of the given assertions
+  at instance location "/type"
+  at evaluate path "/properties/type/anyOf"
+error: The target is expected to match all of the given assertions
+  at instance location ""
+  at evaluate path "/properties"
+EOF
+
+diff "$TMP/output.txt" "$TMP/expected.txt"
+echo "PASS" 1>&2
