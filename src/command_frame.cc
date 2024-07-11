14bd5f3feb9c89de1de158bde7565e5b115afacd
Suprith KG <suprith7kg@gmail.com>
2024-07-12 04:20:35 +0530

    Follow clang format command_frame.cc

--- src/command_frame.cc
+++ src/command_frame.cc
@@ -8,10 +8,9 @@
 #include "command.h"
 #include "utils.h"
 
-static auto enum_to_string(const sourcemeta::jsontoolkit::ReferenceEntryType type) -> std::string
-{
-  switch (type)
-  {
+static auto enum_to_string(const sourcemeta::jsontoolkit::ReferenceEntryType type)
+    -> std::string {
+  switch (type) {
   case sourcemeta::jsontoolkit::ReferenceEntryType::Resource:
     return "resource";
   case sourcemeta::jsontoolkit::ReferenceEntryType::Anchor:
@@ -23,8 +22,8 @@ static auto enum_to_string(const sourcemeta::jsontoolkit::ReferenceEntryType typ
   }
 }
 
-auto intelligence::jsonschema::cli::frame(const std::span<const std::string> &arguments) -> int
-{
+auto intelligence::jsonschema::cli::frame(const std::span<const std::string> &arguments)
+    -> int {
   const auto options{parse_options(arguments, {"json", "j"})};
   CLI_ENSURE(!options.at("").empty(), "You must pass a JSON Schema as input")
   const sourcemeta::jsontoolkit::JSON schema{
@@ -38,70 +37,70 @@ auto intelligence::jsonschema::cli::frame(const std::span<const std::string> &ar
       .wait();
 
   const auto output_json = options.contains("json") || options.contains("j");
-  if (output_json)
-  {
+  if (output_json) {
     auto output_json_object = sourcemeta::jsontoolkit::JSON::make_object();
     auto frame_json = sourcemeta::jsontoolkit::JSON::make_object();
     auto references_json = sourcemeta::jsontoolkit::JSON::make_object();
 
-    for (const auto &[key, entry] : frame)
-    {
+    for (const auto &[key, entry] : frame) {
       auto frame_entry = sourcemeta::jsontoolkit::JSON::make_object();
-      frame_entry.assign("root", sourcemeta::jsontoolkit::JSON{entry.root.value_or(nullptr)});
+      frame_entry.assign("root",
+                         sourcemeta::jsontoolkit::JSON{entry.root.value_or(nullptr)});
       std::ostringstream pointer_stream;
       sourcemeta::jsontoolkit::stringify(entry.pointer, pointer_stream);
-      frame_entry.assign("pointer", sourcemeta::jsontoolkit::JSON{pointer_stream.str()});
+      frame_entry.assign("pointer",
+                         sourcemeta::jsontoolkit::JSON{pointer_stream.str()});
       frame_entry.assign("base", sourcemeta::jsontoolkit::JSON{entry.base});
       frame_entry.assign("type", sourcemeta::jsontoolkit::JSON{enum_to_string(entry.type)});
       std::ostringstream reference_stream;
       sourcemeta::jsontoolkit::stringify(entry.relative_pointer, reference_stream);
-      frame_entry.assign("relativePointer", sourcemeta::jsontoolkit::JSON{reference_stream.str()});
+      frame_entry.assign("relativePointer",
+                         sourcemeta::jsontoolkit::JSON{reference_stream.str()});
       frame_entry.assign("dialect", sourcemeta::jsontoolkit::JSON{entry.dialect});
       frame_json.assign(key.second, sourcemeta::jsontoolkit::JSON{frame_entry});
     }
     output_json_object.assign("frames", sourcemeta::jsontoolkit::JSON{frame_json});
 
-    for (const auto &[pointer, entry] : references)
-    {
+    for (const auto &[pointer, entry] : references) {
       auto ref_entry = sourcemeta::jsontoolkit::JSON::make_object();
-      ref_entry.assign("type",
-                       sourcemeta::jsontoolkit::JSON{pointer.first == sourcemeta::jsontoolkit::ReferenceType::Dynamic ? "Dynamic" : "Static"});
-      ref_entry.assign("destination", sourcemeta::jsontoolkit::JSON{entry.destination});
-      if (entry.base.has_value())
-      {
+      ref_entry.assign("type", sourcemeta::jsontoolkit::JSON{
+                                   pointer.first ==
+                                           sourcemeta::jsontoolkit::ReferenceType::Dynamic
+                                       ? "Dynamic"
+                                       : "Static"});
+      ref_entry.assign("destination",
+                       sourcemeta::jsontoolkit::JSON{entry.destination});
+      if (entry.base.has_value()) {
         ref_entry.assign("base", sourcemeta::jsontoolkit::JSON{entry.base.value()});
       }
-      if (entry.fragment.has_value())
-      {
-        ref_entry.assign("fragment", sourcemeta::jsontoolkit::JSON{entry.fragment.value()});
+      if (entry.fragment.has_value()) {
+        ref_entry.assign("fragment",
+                         sourcemeta::jsontoolkit::JSON{entry.fragment.value()});
       }
       std::ostringstream ref_entry_stream;
       sourcemeta::jsontoolkit::stringify(pointer.second, ref_entry_stream);
-      references_json.assign(ref_entry_stream.str(), sourcemeta::jsontoolkit::JSON{ref_entry});
+      references_json.assign(ref_entry_stream.str(),
+                             sourcemeta::jsontoolkit::JSON{ref_entry});
     }
-    output_json_object.assign("references", sourcemeta::jsontoolkit::JSON{references_json});
+    output_json_object.assign("references",
+                              sourcemeta::jsontoolkit::JSON{references_json});
 
     std::ostringstream print_stream;
     sourcemeta::jsontoolkit::prettify(output_json_object, print_stream);
     std::cout << print_stream.str() << std::endl;
-  }
-  else
-  {
-    for (const auto &[key, entry] : frame)
-    {
+  } else {
+    for (const auto &[key, entry] : frame) {
       std::cout << "(LOCATION) URI: " << key.second << "\n";
       std::cout << "    Schema           : " << entry.root.value_or("<ANONYMOUS>") << "\n";
       std::cout << "    Pointer          :";
-      if (!entry.pointer.empty())
-      {
+      if (!entry.pointer.empty()) {
         std::cout << " ";
       }
       sourcemeta::jsontoolkit::stringify(entry.pointer, std::cout);
       std::cout << "\n";
       std::cout << "    Base URI         : " << entry.base << "\n";
       std::cout << "    Relative Pointer :";
-      if (!entry.relative_pointer.empty())
-      {
+      if (!entry.relative_pointer.empty()) {
         std::cout << " ";
       }
       sourcemeta::jsontoolkit::stringify(entry.relative_pointer, std::cout);
@@ -109,21 +108,22 @@ auto intelligence::jsonschema::cli::frame(const std::span<const std::string> &ar
       std::cout << "    Dialect          : " << entry.dialect << "\n";
     }
 
-    for (const auto &[pointer, entry] : references)
-    {
+    for (const auto &[pointer, entry] : references) {
       std::cout << "(REFERENCE) URI: ";
       sourcemeta::jsontoolkit::stringify(pointer.second, std::cout);
       std::cout << "\n";
-      std::cout << "    Type             : " << (pointer.first == sourcemeta::jsontoolkit::ReferenceType::Dynamic ? "Dynamic" : "Static") << "\n";
+      std::cout << "    Type             : "
+                << (pointer.first == sourcemeta::jsontoolkit::ReferenceType::Dynamic
+                        ? "Dynamic"
+                        : "Static")
+                << "\n";
       std::cout << "    Destination      : " << entry.destination << "\n";
 
-      if (entry.base.has_value())
-      {
+      if (entry.base.has_value()) {
         std::cout << "    - (w/o fragment) : " << entry.base.value() << "\n";
       }
 
-      if (entry.fragment.has_value())
-      {
+      if (entry.fragment.has_value()) {
         std::cout << "    - (fragment)     : " << entry.fragment.value() << "\n";
       }
     }
