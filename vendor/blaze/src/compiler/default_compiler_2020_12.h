8c45d5cd6745759fa7093ddb4b223cfc42bfd937
Juan Cruz Viotti <jv@jviotti.com>
2024-10-17 20:34:35 -0400

    Fix internal export header conflict during build
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/blaze/src/compiler/default_compiler_2020_12.h
+++ vendor/blaze/src/compiler/default_compiler_2020_12.h
@@ -15,7 +15,7 @@ auto compiler_2020_12_applicator_prefixitems(
     const DynamicContext &dynamic_context) -> Template {
   return compiler_draft4_applicator_items_array(
       context, schema_context, dynamic_context,
-      context.uses_unevaluated_items || context.mode == Mode::Exhaustive);
+      context.mode == Mode::Exhaustive, context.uses_unevaluated_items);
 }
 
 auto compiler_2020_12_applicator_items(const Context &context,
@@ -29,16 +29,16 @@ auto compiler_2020_12_applicator_items(const Context &context,
 
   return compiler_draft4_applicator_additionalitems_from_cursor(
       context, schema_context, dynamic_context, cursor,
-      context.uses_unevaluated_items || context.mode == Mode::Exhaustive);
+      context.mode == Mode::Exhaustive, context.uses_unevaluated_items);
 }
 
 auto compiler_2020_12_applicator_contains(const Context &context,
                                           const SchemaContext &schema_context,
                                           const DynamicContext &dynamic_context)
     -> Template {
-  return compiler_2019_09_applicator_contains_conditional_annotate(
+  return compiler_2019_09_applicator_contains_with_options(
       context, schema_context, dynamic_context,
-      context.uses_unevaluated_items || context.mode == Mode::Exhaustive);
+      context.mode == Mode::Exhaustive, context.uses_unevaluated_items);
 }
 
 auto compiler_2020_12_core_dynamicref(const Context &context,
