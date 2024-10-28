d82f3d9348fc5cea4a530f3231d36ed822a4fa7b
Juan Cruz Viotti <jv@jviotti.com>
2024-10-28 17:35:02 -0400

    Fix segmentation error during validation reporting in `metaschema` (#183)
    See: https://github.com/sourcemeta/jsonschema/issues/182
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/metaschema/fail_single.sh
+++ test/metaschema/fail_single.sh
@@ -20,13 +20,13 @@ test "$CODE" = "1" || exit 1
 cat << EOF > "$TMP/expected.txt"
 fail: $(realpath "$TMP")/schema.json
 error: Schema validation failure
-  The string value "object" was expected to equal one of the following values: "array", "boolean", "integer", "null", "number", "object", and "string"
+  The integer value 1 was expected to equal one of the following values: "array", "boolean", "integer", "null", "number", "object", and "string"
     at instance location "/type"
     at evaluate path "/properties/type/anyOf/0/\$ref/enum"
   The value was expected to consist of an array of at least 1 item
     at instance location "/type"
     at evaluate path "/properties/type/anyOf/1/type"
-  The string value was expected to validate against at least one of the 2 given subschemas
+  The integer value was expected to validate against at least one of the 2 given subschemas
     at instance location "/type"
     at evaluate path "/properties/type/anyOf"
   The object value was expected to validate against the 33 defined properties subschemas
