60592c4f0905e463eb7e8cfd7709970ab8ad24a0
pavankumar07s <pawankumar14662693@gmail.com>
2025-02-02 14:59:11 +0530

    done with FindTermColor

--- cmake/FindTermColor.cmake
+++ cmake/FindTermColor.cmake
@@ -6,7 +6,7 @@ add_library(termcolor INTERFACE)
 add_library(termcolor::termcolor ALIAS termcolor)
 
 target_include_directories(termcolor INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/../vendorpool/termcolor/include)
+    ${CMAKE_CURRENT_LIST_DIR}/../vendor/termcolor/include)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TermColor
