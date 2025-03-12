1c0323d413a2cbdb960dff646ca689629405b119
KaranPalan <143683619+Karan-Palan@users.noreply.github.com>
2025-03-12 21:25:29 +0530

    Add a `--json` option to the `lint` command (#228)
    Signed-off-by: karan-palan <karanpalan007@gmail.com>
    Signed-off-by: KaranPalan  <143683619+Karan-Palan@users.noreply.github.com>
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>
    Co-authored-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/command_lint.cc
+++ src/command_lint.cc
@@ -3,16 +3,18 @@
 #include <sourcemeta/core/jsonpointer.h>
 #include <sourcemeta/core/jsonschema.h>
 
-#include <cstdlib>  // EXIT_SUCCESS
+#include <cstdlib>  // EXIT_SUCCESS, EXIT_FAILURE
 #include <fstream>  // std::ofstream
 #include <iostream> // std::cerr, std::cout
+#include <sstream>  // std::ostringstream
 
 #include "command.h"
 #include "utils.h"
 
 auto sourcemeta::jsonschema::cli::lint(
     const std::span<const std::string> &arguments) -> int {
-  const auto options{parse_options(arguments, {"f", "fix"})};
+  const auto options{parse_options(arguments, {"f", "fix", "json", "j"})};
+  const bool output_json = options.contains("json") || options.contains("j");
 
   sourcemeta::core::SchemaTransformer bundle;
   sourcemeta::core::add(bundle,
@@ -27,6 +29,7 @@ auto sourcemeta::jsonschema::cli::lint(
                         sourcemeta::core::AlterSchemaCategory::SyntaxSugar);
 
   bool result{true};
+  auto errors_array = sourcemeta::core::JSON::make_array();
 
   if (options.contains("f") || options.contains("fix")) {
     for (const auto &entry :
@@ -47,28 +50,47 @@ auto sourcemeta::jsonschema::cli::lint(
                                  sourcemeta::core::schema_format_compare);
       output << "\n";
     }
-  } else {
-    for (const auto &entry :
-         for_each_json(options.at(""), parse_ignore(options),
-                       parse_extensions(options))) {
-      log_verbose(options) << "Linting: " << entry.first.string() << "\n";
-      const bool subresult = bundle.check(
-          entry.second, sourcemeta::core::schema_official_walker,
-          resolver(options),
-          [&entry](const auto &pointer, const auto &name, const auto &message) {
+  }
+  for (const auto &entry : for_each_json(options.at(""), parse_ignore(options),
+                                         parse_extensions(options))) {
+    log_verbose(options) << "Linting: " << entry.first.string() << "\n";
+    const bool subresult = bundle.check(
+        entry.second, sourcemeta::core::schema_official_walker,
+        resolver(options),
+        [&](const auto &pointer, const auto &name, const auto &message) {
+          if (output_json) {
+            auto error_obj = sourcemeta::core::JSON::make_object();
+
+            error_obj.assign("path",
+                             sourcemeta::core::JSON{entry.first.string()});
+            error_obj.assign("id", sourcemeta::core::JSON{name});
+            error_obj.assign("message", sourcemeta::core::JSON{message});
+
+            std::ostringstream pointer_stream;
+            sourcemeta::core::stringify(pointer, pointer_stream);
+            error_obj.assign("schemaLocation",
+                             sourcemeta::core::JSON{pointer_stream.str()});
+
+            errors_array.push_back(error_obj);
+          } else {
             std::cout << entry.first.string() << ":\n";
             std::cout << "  " << message << " (" << name << ")\n";
             std::cout << "    at schema location \"";
             sourcemeta::core::stringify(pointer, std::cout);
             std::cout << "\"\n";
-          });
+          }
+        });
 
-      if (subresult) {
-        log_verbose(options) << "PASS: " << entry.first.string() << "\n";
-      } else {
-        result = false;
-      }
+    if (!subresult) {
+      result = false;
     }
+  }
+  if (output_json) {
+    auto output_json_object = sourcemeta::core::JSON::make_object();
+    output_json_object.assign("valid", sourcemeta::core::JSON{result});
+    output_json_object.assign("errors", sourcemeta::core::JSON{errors_array});
+    sourcemeta::core::prettify(output_json_object, std::cout);
+    std::cout << "\n";
   }
 
   return result ? EXIT_SUCCESS : EXIT_FAILURE;
---
+++ test/lint/pass_lint_fix_json.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.json"
+{
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "type": "string",
+  "enum": [ "foo" ]
+}
+EOF
+
+"$1" lint "$TMP/schema.json" --fix --json >"$TMP/output.json" 2>&1
+
+cat << EOF > "$TMP/expected.json"
+{
+  "valid": true,
+  "errors": []
+}
+EOF
+
+diff "$TMP/output.json" "$TMP/expected.json"
+
+cat << 'EOF' > "$TMP/expected_fixed.json"
+{
+  "$schema": "http://json-schema.org/draft-06/schema#",
+  "const": "foo"
+}
+EOF
+
+diff "$TMP/schema.json" "$TMP/expected_fixed.json"
