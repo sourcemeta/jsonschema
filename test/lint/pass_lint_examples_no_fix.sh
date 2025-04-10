33e1666b9e50a0b49d42ea4a899f9942460156d8
Juan Cruz Viotti <jv@jviotti.com>
2025-04-10 15:53:55 -0400

    Validate `examples` in the `lint` command (#288)
    Fixes: https://github.com/sourcemeta/jsonschema/issues/276
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/lint/pass_lint_examples_no_fix.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.json"
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "foo": {
+      "type": "string",
+      "examples": [ "foo" ]
+    }
+  }
+}
+EOF
+
+"$1" lint "$TMP/schema.json" > "$TMP/result.txt" 2>&1
+
+cat << 'EOF' > "$TMP/output.txt"
+EOF
+
+diff "$TMP/result.txt" "$TMP/output.txt"
+
+cat << 'EOF' > "$TMP/expected.json"
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "foo": {
+      "type": "string",
+      "examples": [ "foo" ]
+    }
+  }
+}
+EOF
+
+diff "$TMP/schema.json" "$TMP/expected.json"
