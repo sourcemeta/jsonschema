9759a4c1eba6c70aa4e78da356e011412112e61e
Juan Cruz Viotti <jv@jviotti.com>
2025-04-21 14:18:27 -0400

    Warn when attempting to resolve a schema without identifiers (#311)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/core/src/core/jsonschema/resolver.cc
+++ vendor/core/src/core/jsonschema/resolver.cc
@@ -15,7 +15,7 @@ SchemaMapResolver::SchemaMapResolver(const SchemaResolver &resolver)
 auto SchemaMapResolver::add(const JSON &schema,
                             const std::optional<std::string> &default_dialect,
                             const std::optional<std::string> &default_id)
-    -> void {
+    -> bool {
   assert(sourcemeta::core::is_schema(schema));
 
   // Registering the top-level schema is not enough. We need to check
@@ -24,6 +24,7 @@ auto SchemaMapResolver::add(const JSON &schema,
   frame.analyse(schema, schema_official_walker, *this, default_dialect,
                 default_id);
 
+  bool added_any_schema{false};
   for (const auto &[key, entry] : frame.locations()) {
     if (entry.type != SchemaFrame::LocationType::Resource) {
       continue;
@@ -58,7 +59,11 @@ auto SchemaMapResolver::add(const JSON &schema,
       error << "Cannot register the same identifier twice: " << key.second;
       throw SchemaError(error.str());
     }
+
+    added_any_schema = true;
   }
+
+  return added_any_schema;
 }
 
 auto SchemaMapResolver::operator()(std::string_view identifier) const
