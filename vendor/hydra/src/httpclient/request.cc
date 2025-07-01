12a5c8be03d993b7b09a16a702125127e58040b6
Juan Cruz Viotti <jv@jviotti.com>
2025-07-01 09:39:14 -0400

    Support HTTP redirects when using the `--http` option
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/hydra/src/httpclient/request.cc
+++ vendor/hydra/src/httpclient/request.cc
@@ -87,8 +87,26 @@ auto ClientRequest::send(std::istream &body) -> std::future<ClientResponse> {
 }
 
 auto ClientRequest::send() -> std::future<ClientResponse> {
-  static std::istringstream empty_body;
-  return this->send(empty_body);
+  std::ostringstream output;
+  this->stream.on_data(
+      [&output](const Status, std::span<const std::uint8_t> buffer) noexcept {
+        std::copy(buffer.begin(), buffer.end(),
+                  std::ostream_iterator<char>(output));
+      });
+
+  std::map<std::string, std::string> headers;
+  this->stream.on_header([&headers, this](const Status, std::string_view key,
+                                          std::string_view value) noexcept {
+    std::string header{key};
+    if (this->capture_.contains(header) || this->capture_all_) {
+      headers.insert_or_assign(std::move(header), std::string{value});
+    }
+  });
+
+  std::promise<ClientResponse> response;
+  response.set_value(
+      {this->stream.send().get(), std::move(headers), std::move(output)});
+  return response.get_future();
 }
 
 } // namespace sourcemeta::hydra::http
