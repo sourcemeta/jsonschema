ea9c0efcc282572c629efcc884baae2d53d9eb7e
Juan Cruz Viotti <jv@jviotti.com>
2025-07-01 14:12:22 -0400

    Upgrade Core and Hydra to the latest versions
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/FindHydra.cmake
+++ cmake/FindHydra.cmake
@@ -1,7 +1,5 @@
 if(NOT Hydra_FOUND)
   set(HYDRA_INSTALL OFF CACHE BOOL "disable installation")
-  set(HYDRA_HTTPSERVER OFF CACHE BOOL "disable the Hydra HTTP server module")
-  set(HYDRA_BUCKET OFF CACHE BOOL "disable the Hydra bucket module")
   add_subdirectory("${PROJECT_SOURCE_DIR}/vendor/hydra")
   set(Hydra_FOUND ON)
 endif()
--- vendor/hydra/src/http/include/sourcemeta/hydra/http.h
+++ vendor/hydra/src/http/include/sourcemeta/hydra/http.h
@@ -1,9 +1,8 @@
 #ifndef SOURCEMETA_HYDRA_HTTP_H
 #define SOURCEMETA_HYDRA_HTTP_H
 
-/// @defgroup http HTTP Common
-/// @brief Common aspects of the HTTP protocol implementation for both client
-/// and server
+/// @defgroup http HTTP
+/// @brief An client implementation of the HTTP protocol with streaming support
 ///
 /// This functionality is included as follows:
 ///
@@ -15,7 +14,10 @@
 #include <sourcemeta/hydra/http_header.h>
 #include <sourcemeta/hydra/http_method.h>
 #include <sourcemeta/hydra/http_mime.h>
+#include <sourcemeta/hydra/http_request.h>
+#include <sourcemeta/hydra/http_response.h>
 #include <sourcemeta/hydra/http_status.h>
+#include <sourcemeta/hydra/http_stream.h>
 #include <sourcemeta/hydra/http_time.h>
 
 #endif
