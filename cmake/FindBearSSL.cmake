495cedc6f198884dab7a7a81bfc4f4fda0cd3365
Juan Cruz Viotti <jv@jviotti.com>
2025-08-11 13:02:04 -0400

    Upgrade Core to `2951f08dadba6003aea55a28e2871cd04c5adb75` (#434)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/FindBearSSL.cmake
+++ cmake/FindBearSSL.cmake
@@ -330,33 +330,5 @@ if(NOT BearSSL_FOUND)
       WINDOWS_EXPORT_ALL_SYMBOLS TRUE
       EXPORT_NAME bearssl)
 
-  if(SOURCEMETA_CORE_INSTALL)
-    include(GNUInstallDirs)
-    install(TARGETS bearssl
-      EXPORT bearssl
-      PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-        COMPONENT sourcemeta_core_dev
-      PRIVATE_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-        COMPONENT sourcemeta_core_dev
-      RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
-        COMPONENT sourcemeta_core
-      LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-        COMPONENT sourcemeta_core
-        NAMELINK_COMPONENT sourcemeta_core_dev
-      ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-        COMPONENT sourcemeta_core_dev)
-    install(EXPORT bearssl
-      DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/bearssl"
-      COMPONENT sourcemeta_core_dev)
-
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/bearssl-config.cmake
-      "include(\"\${CMAKE_CURRENT_LIST_DIR}/bearssl.cmake\")\n"
-      "check_required_components(\"bearssl\")\n")
-    install(FILES
-      "${CMAKE_CURRENT_BINARY_DIR}/bearssl-config.cmake"
-      DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/bearssl"
-      COMPONENT sourcemeta_core_dev)
-  endif()
-
   set(BearSSL_FOUND ON)
 endif()
