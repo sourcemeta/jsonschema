caf4a5e9d105fb5bfd9bfd13cffdd5664b8b64c0
Juan Cruz Viotti <jv@jviotti.com>
2025-09-12 09:26:26 -0400

    Fix `validate --template` crashing with `patternProperties`
    Fixes: https://github.com/sourcemeta/jsonschema/issues/476
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/compile/pass_patternproperties.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.json"
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "patternProperties": {
+    "[\\-]": {
+      "type": "string"
+    }
+  }
+}
+EOF
+
+cat << 'EOF' > "$TMP/instance.json"
+{}
+EOF
+
+"$1" compile "$TMP/schema.json" > "$TMP/template.json"
+"$1" validate --template "$TMP/template.json" "$TMP/schema.json" "$TMP/instance.json"
