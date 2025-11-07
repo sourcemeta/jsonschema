5312fbb3d987dbe8ca6bb5d7c417480119b7a2eb
Juan Cruz Viotti <jv@jviotti.com>
2025-11-07 13:03:15 -0400

    Improve error output when dealing with conditional operators (#532)
    Fixes: https://github.com/sourcemeta/jsonschema/issues/530
    Fixes: https://github.com/sourcemeta/jsonschema/issues/411
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/metaschema/fail_json.sh
+++ test/metaschema/fail_json.sh
@@ -22,6 +22,27 @@ cat << EOF > "$TMP/expected.json"
   "valid": false,
   "errors": [
     {
+      "keywordLocation": "/properties/type/anyOf/0/\$ref/enum",
+      "absoluteKeywordLocation": "http://json-schema.org/draft-04/schema#/definitions/simpleTypes/enum",
+      "instanceLocation": "/type",
+      "instancePosition": [ 3, 3, 3, 11 ],
+      "error": "The integer value 1 was expected to equal one of the following values: \"array\", \"boolean\", \"integer\", \"null\", \"number\", \"object\", and \"string\""
+    },
+    {
+      "keywordLocation": "/properties/type/anyOf/0/\$ref",
+      "absoluteKeywordLocation": "http://json-schema.org/draft-04/schema#/properties/type/anyOf/0/\$ref",
+      "instanceLocation": "/type",
+      "instancePosition": [ 3, 3, 3, 11 ],
+      "error": "The integer value was expected to validate against the statically referenced schema"
+    },
+    {
+      "keywordLocation": "/properties/type/anyOf/1/type",
+      "absoluteKeywordLocation": "http://json-schema.org/draft-04/schema#/properties/type/anyOf/1/type",
+      "instanceLocation": "/type",
+      "instancePosition": [ 3, 3, 3, 11 ],
+      "error": "The value was expected to be of type array but it was of type integer"
+    },
+    {
       "keywordLocation": "/properties/type/anyOf",
       "absoluteKeywordLocation": "http://json-schema.org/draft-04/schema#/properties/type/anyOf",
       "instanceLocation": "/type",
