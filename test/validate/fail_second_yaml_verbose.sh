44e4d09db1c34fb00d20a33f9fe180f36f53757e
Robin H. Johnson <rjohnson@coreweave.com>
2025-11-24 11:27:05 -0800

    test: Start YAML testcases for multi-doc & other edges
    Signed-off-by: Robin H. Johnson <rjohnson@coreweave.com>

---
+++ test/validate/fail_second_yaml_verbose.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.yaml"
+$schema: https://json-schema.org/draft/2019-09/schema
+type: object
+properties:
+  foo:
+    type: string
+EOF
+
+cat << 'EOF' > "$TMP/instance.yaml"
+---
+foo: "good"
+---
+foo: 1
+EOF
+
+"$1" validate "$TMP/schema.yaml" "$TMP/instance.yaml" 2> "$TMP/stderr.txt" \
+  && EXIT_CODE="$?" || EXIT_CODE="$?"
+test "$EXIT_CODE" = "2" || exit 1
+
+cat << EOF > "$TMP/expected.txt"
+fail: $(realpath "$TMP")/instance.yaml
+error: Schema validation failure
+  The value was expected to be of type string but it was of type integer
+    at instance location "/foo" (line 1, column 1)
+    at evaluate path "/properties/foo/type"
+  The object value was expected to validate against the single defined property subschema
+    at instance location "" (line 1, column 1)
+    at evaluate path "/properties"
+EOF
+
+diff -NuarwbB "$TMP/stderr.txt" "$TMP/expected.txt"
+
---
+++ test/validate/pass_yaml.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+TMP="$(mktemp -d)"
+clean() { rm -rf "$TMP"; }
+trap clean EXIT
+
+cat << 'EOF' > "$TMP/schema.yaml"
+$schema: https://json-schema.org/draft/2019-09/schema
+type: object
+properties:
+  foo:
+    type: string
+EOF
+
+cat << 'EOF' > "$TMP/instance.yaml"
+foo: "test"
+EOF
+
+"$1" validate "$TMP/schema.yaml" "$TMP/instance.yaml" 2> "$TMP/stderr.txt" \
+  && EXIT_CODE="$?" || EXIT_CODE="$?"
+test "$EXIT_CODE" = "0" || exit 1
+
+cat << EOF > "$TMP/expected.txt"
+EOF
+
+diff "$TMP/stderr.txt" "$TMP/expected.txt"
+
