4728223dfe724760c9098bfd03c35ea9f0b52611
Juan Cruz Viotti <jv@jviotti.com>
2025-12-02 12:31:44 -0400

    Expose a spawning interface in the CLI NPM wrapper (#564)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ npm/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const { spawn } = require('./main.js');
+
+spawn(process.argv.slice(2), { stdio: 'inherit' })
+  .then((result) => {
+    process.exit(result.code);
+  })
+  .catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
