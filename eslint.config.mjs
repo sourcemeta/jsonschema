44c4cd8d1eeffdaecd5c06720b5024ce131e9df1
Juan Cruz Viotti <jv@jviotti.com>
2025-12-02 12:07:35 -0400

    Take NPM packaging logic out of `npm-deploy.sh` (#563)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+]);
