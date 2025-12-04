66c700cef547d9835c85dcd154f5c236da0c47c9
Juan Cruz Viotti <jv@jviotti.com>
2025-12-04 08:50:31 -0400

    Avoid synchronous functions in the NPM wrapper (#575)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- npm/main.js
+++ npm/main.js
@@ -1,6 +1,6 @@
 const os = require('os');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 const child_process = require('child_process');
 
 const PLATFORM = os.platform() === 'win32' ? 'windows' : os.platform();
@@ -9,31 +9,9 @@ const EXTENSION = PLATFORM === 'windows' ? '.exe' : '';
 const EXECUTABLE = path.join(__dirname, '..', 'build', 'github-releases',
   `jsonschema-${PLATFORM}-${ARCH}${EXTENSION}`);
 
-function spawn(args, options = {}) {
-  const json = options.json === true;
-  const spawnArgs = json ? [...args, '--json'] : args;
-
+function spawnProcess(executable, args, options) {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(EXECUTABLE)) {
-      reject(new Error(
-        `The JSON Schema CLI NPM package does not support ${os.platform()} for ${os.arch()} yet. ` +
-        'Please open a GitHub issue at https://github.com/sourcemeta/jsonschema'
-      ));
-      return;
-    }
-
-    if (PLATFORM === 'darwin') {
-      child_process.spawnSync('/usr/bin/xattr', ['-c', EXECUTABLE], { stdio: 'inherit' });
-    }
-
-    const spawnOptions = {
-      windowsHide: true,
-      ...options
-    };
-
-    delete spawnOptions.json;
-
-    const process = child_process.spawn(EXECUTABLE, spawnArgs, spawnOptions);
+    const process = child_process.spawn(executable, args, options);
 
     let stdout = '';
     let stderr = '';
@@ -55,13 +33,42 @@ function spawn(args, options = {}) {
     });
 
     process.on('close', (code) => {
-      resolve({
-        code: code,
-        stdout: json ? JSON.parse(stdout) : stdout,
-        stderr: stderr
-      });
+      resolve({ code, stdout, stderr });
     });
   });
+}
+
+async function spawn(args, options = {}) {
+  const json = options.json === true;
+  const spawnArgs = json ? [...args, '--json'] : args;
+
+  try {
+    await fs.access(EXECUTABLE);
+  } catch {
+    throw new Error(
+      `The JSON Schema CLI NPM package does not support ${os.platform()} for ${os.arch()} yet. ` +
+      'Please open a GitHub issue at https://github.com/sourcemeta/jsonschema'
+    );
+  }
+
+  if (PLATFORM === 'darwin') {
+    await spawnProcess('/usr/bin/xattr', ['-c', EXECUTABLE], { stdio: 'inherit' });
+  }
+
+  const spawnOptions = {
+    windowsHide: true,
+    ...options
+  };
+
+  delete spawnOptions.json;
+
+  const result = await spawnProcess(EXECUTABLE, spawnArgs, spawnOptions);
+
+  return {
+    code: result.code,
+    stdout: json ? JSON.parse(result.stdout) : result.stdout,
+    stderr: result.stderr
+  };
 }
 
 module.exports = { spawn };
