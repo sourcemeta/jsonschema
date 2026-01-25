216719a105205a1f4f1f33cb7f963b81030c1617
karan-palan <karanpalan007@gmail.com>
2026-01-25 19:05:56 +0530

    feat: add a message about studio on install on npm, pypi, install script and jsonschema --help
    Signed-off-by: karan-palan <karanpalan007@gmail.com>

---
+++ npm/postinstall.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.log('');
+console.log('\u{1F4A1} Try the Sourcemeta Studio VS Code extension for an enhanced experience!');
+console.log('   Open in VS Code: vscode:extension/sourcemeta.sourcemeta-studio');
+console.log('   Or visit: https://marketplace.visualstudio.com/items?itemName=sourcemeta.sourcemeta-studio');
+console.log('');
