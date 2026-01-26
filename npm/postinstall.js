18c94cfb9bb74c4d74a72c14bb9897ee1b9fb9a6
KaranPalan <143683619+Karan-Palan@users.noreply.github.com>
2026-01-26 21:33:27 +0530

    Add VS code extension promotion message on install across various distributions (#628)
    Signed-off-by: karan-palan <karanpalan007@gmail.com>

---
+++ npm/postinstall.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+if (process.stdout.isTTY) {
+  console.log('');
+  console.log('Tip: Try the Sourcemeta Studio VS Code extension for an enhanced experience!');
+  console.log('     Open in VS Code: vscode:extension/sourcemeta.sourcemeta-studio');
+  console.log('     Or visit: https://marketplace.visualstudio.com/items?itemName=sourcemeta.sourcemeta-studio');
+  console.log('');
+}
